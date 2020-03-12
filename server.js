fd2749b53aaa50fca86557ed19953779581d6064
Mr4leksandr <aleksandrklemets15@gmail.com>
2020-03-12 16:41:26 +0200

    asg

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
     //console.log("Path", path);
 });
